ac1aa53c1ea250f039fc6f3649f2bbb15c0d7083
rusya <dnepr.ukrainec@gmail.com>
2020-01-08 16:38:03 +0200

    class

---
+++ ConsoleApp73/Jsefef.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp73
+{
+    class Jsefef
+    {
+        public Jsefef()
+        {
+
+        }
+    }
+}
